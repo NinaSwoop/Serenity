beaa1afb0f02cfa250ed632d136f68c1dd0028b0
busy-gnl <vallantjesse@live.com>
2022-12-14 15:02:39 +0100

    deleted entity Content

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -6,23 +6,52 @@ use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
-class CategoryFixture extends Fixture
+class CategoryFixtures extends Fixture
 {
     private const CATEGORIES = [
-        'Comprendre mon opération',
-        'Finir les démarches administratives',
-        'Préparer mon arrivée en toute sérénité',
-        'Anticiper ma sortie',
-        'Ma check-list avant le départ à la clinique'
+
+        [
+            'title' => "Comprendre mon opération",
+            'progressColor' => "category1-progress",
+            'progressbarColor' => "category1-progressbar",
+
+        ],
+        [
+            'title' => "Finir les démarches administratives",
+            'progressColor' => "category2-progress",
+            'progressbarColor' => "category2-progressbar",
+
+        ],
+        [
+            'title' => "Préparer mon arrivée en toute sérénité",
+            'progressColor' => "category3-progress",
+            'progressbarColor' => "category3-progressbar",
+
+        ],
+        [
+            'title' => "Anticiper ma sortie",
+            'progressColor' => "category4-progress",
+            'progressbarColor' => "category4-progressbar",
+
+        ],
+        [
+            'title' => 'Ma check-list avant le départ à la clinique',
+            'progressColor' => 'category5-progress',
+            'progressbarColor' => 'category5-progressbar',
+
+        ],
     ];
+
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::CATEGORIES as $categoryName) {
+        foreach (self::CATEGORIES as $key => $values) {
             $category = new Category();
-            $category->setTitle($categoryName);
+            $category->setTitle($values['title']);
+            $category->setProgressColor($values['progressColor']);
+            $category->setProgressbarColor($values['progressbarColor']);
             $manager->persist($category);
-            $this->addReference('category_' . $categoryName, $category);
+            $this->addReference('category_' . $key, $category);
         }
         $manager->flush();
     }
