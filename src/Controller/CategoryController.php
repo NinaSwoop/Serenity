429717a8cf96e9cc36c3567c6d82554272ab3a47
busy-gnl <vallantjesse@live.com>
2022-12-30 19:02:55 +0100

    added date and hour of the day

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Category;
-use App\Entity\SchemaContent;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
