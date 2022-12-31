7221f910190d6f605fc999f8b87ec16826191bc2
busy-gnl <vallantjesse@live.com>
2022-12-31 10:34:15 +0100

    fixed a bootstrap issue (a line in scss deleted by mistake)

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -38,7 +38,7 @@ class CategoryController extends AbstractController
 
         return $this->renderForm('category/new.html.twig', [
             'category' => $category,
-            'form' => $form,
+            'form' => $form
         ]);
     }
 
