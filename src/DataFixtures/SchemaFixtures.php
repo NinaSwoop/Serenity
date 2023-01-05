4934ec961ee6225e8c3710148b4fbb784a58cb13
Nina Iacoponelli <nina.iacoponelli@gmail.com>
2023-01-05 23:03:07 +0100

    update SchemaFixtures

--- src/DataFixtures/SchemaFixtures.php
+++ src/DataFixtures/SchemaFixtures.php
@@ -11,25 +11,80 @@ class SchemaFixtures extends Fixture implements DependentFixtureInterface
 {
     public const SCHEMACONTENT = [
         [
-            'picture' => 'Schema1.jpg',
+            'picture' => 'Schema-mater1.jpeg',
             'category' => 'Comprendre mon opération',
-            'secretariat' => ['secretariat_orthopédie', 'secretariat_maternité', 'secretariat_neurologie']
+            'secretariat' => ['secretariat_maternité']
         ],
         [
-            'picture' => 'Schema2.jpg',
+            'picture' => 'Schema-mater2.jpeg',
             'category' => 'Comprendre mon opération',
-            'secretariat' => ['secretariat_orthopédie', 'secretariat_maternité', 'secretariat_neurologie']
+            'secretariat' => ['secretariat_maternité']
         ],
         [
-            'picture' => 'Schema3.jpg',
+            'picture' => 'Schema-mater3.jpeg',
             'category' => 'Comprendre mon opération',
-            'secretariat' => ['secretariat_orthopédie', 'secretariat_maternité', 'secretariat_neurologie']
+            'secretariat' => ['secretariat_maternité']
         ],
         [
-            'picture' => 'Schema4.jpg',
+            'picture' => 'Schema-mater4.jpeg',
             'category' => 'Comprendre mon opération',
-            'secretariat' => ['secretariat_orthopédie', 'secretariat_maternité', 'secretariat_neurologie']
-        ]
+            'secretariat' => ['secretariat_maternité']
+        ],
+        [
+            'picture' => 'Schema-mater5.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_maternité']
+        ],
+        [
+            'picture' => 'Schema-ortho1.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_orthopédie']
+        ],
+        [
+            'picture' => 'Schema-ortho2.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_orthopédie']
+        ],
+        [
+            'picture' => 'Schema-ortho3.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_orthopédie']
+        ],
+        [
+            'picture' => 'Schema-ortho4.jpg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_orthopédie']
+        ],
+        [
+            'picture' => 'Schema-ortho5.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_orthopédie']
+        ],
+        [
+            'picture' => 'Schema-neuro1.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_neurologie']
+        ],
+        [
+            'picture' => 'Schema-neuro2.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_neurologie']
+        ],
+        [
+            'picture' => 'Schema-neuro3.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_neurologie']
+        ],
+        [
+            'picture' => 'Schema-neuro4.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_neurologie']
+        ],
+        [
+            'picture' => 'Schema-neuro5.jpeg',
+            'category' => 'Comprendre mon opération',
+            'secretariat' => ['secretariat_neurologie']
+        ],
     ];
 
     public function load(ObjectManager $manager): void
