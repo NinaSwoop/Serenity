3db7fb157821bee3f401eadaa44f4a4fc611d445
Kévin Albespy <kevin.albespy@gmail.com>
2023-01-06 15:45:49 +0100

    Update of SchemaFixtures to add title fixtures

--- src/DataFixtures/SchemaFixtures.php
+++ src/DataFixtures/SchemaFixtures.php
@@ -13,76 +13,91 @@ class SchemaFixtures extends Fixture implements DependentFixtureInterface
         [
             'picture' => 'Schema-mater1.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Césarienne',
             'secretariat' => ['secretariat_maternité']
         ],
         [
             'picture' => 'Schema-mater2.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Croissance in utero',
             'secretariat' => ['secretariat_maternité']
         ],
         [
             'picture' => 'Schema-mater3.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Anatomie',
             'secretariat' => ['secretariat_maternité']
         ],
         [
             'picture' => 'Schema-mater4.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Yoga prénatal',
             'secretariat' => ['secretariat_maternité']
         ],
         [
             'picture' => 'Schema-mater5.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Anesthésie Péridurale',
             'secretariat' => ['secretariat_maternité']
         ],
         [
             'picture' => 'Schema-ortho1.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Examen orthopédique',
             'secretariat' => ['secretariat_orthopédie']
         ],
         [
             'picture' => 'Schema-ortho2.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Utiliser une béquille orthopédique',
             'secretariat' => ['secretariat_orthopédie']
         ],
         [
             'picture' => 'Schema-ortho3.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Rééducation post-op',
             'secretariat' => ['secretariat_orthopédie']
         ],
         [
             'picture' => 'Schema-ortho4.jpg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Kinésithérapie des membres inférieurs',
             'secretariat' => ['secretariat_orthopédie']
         ],
         [
             'picture' => 'Schema-ortho5.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Rester mobile',
             'secretariat' => ['secretariat_orthopédie']
         ],
         [
             'picture' => 'Schema-neuro1.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Comprendre votre cerveau',
             'secretariat' => ['secretariat_neurologie']
         ],
         [
             'picture' => 'Schema-neuro2.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Les nouvelles procédures',
             'secretariat' => ['secretariat_neurologie']
         ],
         [
             'picture' => 'Schema-neuro3.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Examens Neurologiques',
             'secretariat' => ['secretariat_neurologie']
         ],
         [
             'picture' => 'Schema-neuro4.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'IRM Cérébrale',
             'secretariat' => ['secretariat_neurologie']
         ],
         [
             'picture' => 'Schema-neuro5.jpeg',
             'category' => 'Comprendre mon opération',
+            'title' => 'Pathologies Cérébrales',
             'secretariat' => ['secretariat_neurologie']
         ],
     ];
@@ -92,7 +107,7 @@ class SchemaFixtures extends Fixture implements DependentFixtureInterface
         foreach (self::SCHEMACONTENT as $values) {
             $schemaContent = new SchemaContent();
             $schemaContent->setPicture($values['picture']);
-            $schemaContent->setTitle($values['picture']);
+            $schemaContent->setTitle($values['title']);
             $schemaContent->setCategory($this->getReference('category_' . $values['category']));
             foreach ($values['secretariat'] as $secretariat) {
                 $schemaContent->addSecretariat($this->getReference($secretariat));
