2ffcbcaa5f63ab8f794d0750001293f89202daa7
busy-gnl <vallantjesse@live.com>
2023-01-18 19:11:44 +0100

    added Role Hierarchy in security.yaml

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_USER')]
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
