7b9461bdd702414c210ea65c68d1cd8907e7d149
Kévin Albespy <103110827+kalbespy@users.noreply.github.com>
2023-01-27 10:11:14 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,8 +13,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
-use function PHPUnit\Framework\throwException;
-
 #[Route('/user')]
 class UserController extends AbstractController
 {
